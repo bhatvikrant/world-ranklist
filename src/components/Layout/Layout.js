0e77600c5d8ffe8e42958b91626dc0982d6000b5
Vikrant Bhat <vik.bhat223@gmail.com>
2020-11-10 03:04:33 +0530

    layout , styles improved

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,40 @@
+import {useState, useEffect} from 'react'
+
 import Head from "next/head";
 import Link from "next/link";
 
 // STYLES
 import styles from "./Layout.module.css";
 
+// ICONS
+import { Brightness6Rounded } from "@material-ui/icons";
+
 // LOGO
 import Logo from "./logo.svg";
 
 const Layout = ({ children, title = "World Ranklist" }) => {
+
+	const [theme, setTheme] = useState('light');
+			
+
+	useEffect(() =>{
+document.documentElement.setAttribute('data-theme', localStorage.getItem('theme'))
+setTheme(localStorage.getItem('theme'));
+	}, [])
+
+	const switchTheme = () => {
+		if(theme ==="light") {
+			saveTheme('dark')
+		}else{
+			saveTheme('light')
+		}
+	}
+
+	const saveTheme = theme => {
+		setTheme(theme)
+			localStorage.setItem('theme', theme)
+			document.documentElement.setAttribute('data-theme', theme)
+	}
 	return (
 		<div className={styles.container}>
 			<Head>
@@ -21,6 +48,10 @@ const Layout = ({ children, title = "World Ranklist" }) => {
 						<Logo />
 					</a>
 				</Link>
+
+				<button className={styles.themeSwitcher} onClick={switchTheme}>
+					<Brightness6Rounded />
+				</button>
 			</header>
 
 			<main className={styles.main}>{children}</main>
