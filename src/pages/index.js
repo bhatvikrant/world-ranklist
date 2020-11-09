9357bdca527403879204ff144e9de6188759401f
Vikrant Bhat <vik.bhat223@gmail.com>
2020-11-09 22:58:51 +0530

    search input filtering added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 // STYLES
 import styles from "../styles/Home.module.css";
 
@@ -8,11 +10,28 @@ import CountriesTable from "../components/CountriesTable/CountriesTable";
 
 export default function Home({ countries }) {
 	// console.log("countries:", countries);
+
+	const [keyword, setKeyword] = useState("");
+
+	const filteredCountries = countries.filter(
+		country =>
+			country.name.toLowerCase().includes(keyword) ||
+			country.region.toLowerCase().includes(keyword) ||
+			country.subregion.toLowerCase().includes(keyword),
+	);
+
+	const onInputChange = e => {
+		e.preventDefault();
+		setKeyword(e.target.value.toLowerCase());
+	};
 	return (
 		<Layout>
 			<div className={styles.counts}>Found {countries.length} countries</div>
-			<SearchInput placeholder="Filter by Name, Region or SubRegion" />
-			<CountriesTable countries={countries} />
+			<SearchInput
+				placeholder="Filter by Name, Region or SubRegion"
+				onChange={onInputChange}
+			/>
+			<CountriesTable countries={filteredCountries} />
 		</Layout>
 	);
 }
